b03ae1ea022ecc047a5dacc5e8a164ebbec5ebbf
wip-abramson <wip.abramson@gmail.com>
2021-05-13 21:59:53 +0100

    add delete endpoint

--- app/routers/oicd_admin.py
+++ app/routers/oicd_admin.py
@@ -12,7 +12,7 @@ LOGGER = logging.getLogger(__name__)
 @router.get(
     "/oidc/admin/vc-configs/",
     status_code=status.HTTP_200_OK,
-    tags=["Verifiable Credential Presentation Configuration"],
+    tags=["OIDC Proof Request Configuration"],
 )
 async def vc_configs(request: Request, response: Response):
     oidc_proof_request = db.query(OIDCProofRequest).all()
@@ -21,7 +21,7 @@ async def vc_configs(request: Request, response: Response):
 
 
 @router.post(
-    "/oidc/admin/vc-configs/", tags=["Verifiable Credential Presentation Configuration"]
+    "/oidc/admin/vc-configs/", tags=["OIDC Proof Request Configuration"]
 )
 async def vc_configs(
     request: Request,
@@ -39,7 +39,7 @@ async def vc_configs(
 
 
 @router.put(
-    "/oidc/admin/vc-configs/", tags=["Verifiable Credential Presentation Configuration"]
+    "/oidc/admin/vc-configs/", tags=["OIDC Proof Request Configuration"], status=204
 )
 # TODO - Fix Update API for Presentation Configurations. Not working at the moment.
 async def vc_configs(
@@ -61,11 +61,19 @@ async def vc_configs(
     LOGGER.info('OIDC Proof Request Updating ', request.body().__str__(), request.headers, request.query_params)
     oidc_proof_request_record.update(updated_oidc_pr)
     db.commit()
-    return status.HTTP_200_OK
+    return
 
 
 @router.delete(
-    "/oidc/admin/vc-configs/", tags=["Verifiable Credential Presentation Configuration"]
+    "/oidc/admin/vc-configs/", tags=["OIDC Proof Request Configuration"], status=204
 )
-async def vc_configs(request: Request, response: Response, id: str = Form(...)):
-    pass
+async def vc_configs(request: Request, response: Response, oidc_scope: str):
+    oidc_proof_request_record = db.query(OIDCProofRequest).filter(OIDCProofRequest.oidc_scope == oidc_scope)
+
+    if not oidc_proof_request_record.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"OIDC Proof Request with scope {oidc_scope} not found")
+
+    oidc_proof_request_record.delete(synchronize_session=False)
+    db.commit()
+    return
